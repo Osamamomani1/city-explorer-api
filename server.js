ec1d3542bbfdaa337229ebd7032917cbc556766d
Osamamomani1 <momaniosama97@gmail.com>
2021-06-30 18:14:01 +0300

    lab9done

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const express = require('express') // require the express package
 const app = express() // initialize your express app instance
 const cors = require('cors');
 require('dotenv').config();
-const weatherData = require('./data/weather.json')
 const axios = require('axios'); // require the package
+const weatherHandler=require('./controller/wather.controler')
+const moviesHandler=require('./controller/movie.controler')
+
 app.use(cors()) // after you initialize your express app instance
-// a server endpoint 
-// // inside your callback function
-// axios.get(url).then(response => response.data).catch(error => console.log(error));
+
 
 app.get('/', // our endpoint name
  function (req, res) { // callback function of what we should do with our request
@@ -18,128 +18,12 @@ app.get('/', // our endpoint name
 
 
 
-class ForeCast{
-  constructor(item){
-      this.date=item.valid_date ,
-      this.description=`Low of : ${item.low_temp} and a high of ${item.max_temp} with a ${item.weather.description}  `
-  }
-}
-
-class Movie {
-  constructor(item) {
-      this.title = item.original_title;
-      this.overview = item.overview;
-      this.averageVotes = item.vote_average;
-      this.totalVotes = item.vote_count;
-      this.popularity = item.popularity;
-      this.releasedOn = item.release_date;
-      if (item.poster_path){
-          this.imageUrl = `https://image.tmdb.org/t/p/w500/${item.poster_path}`;
-      }
-  }
-  
-}
-
-
-
-
-
-
-app.get('/weather', (req, res) => {
-  let searchQuery = req.query.city_name;
-  console.log(searchQuery);
-  let cityData= weatherData.find((item)=>{
-    if (searchQuery.toLowerCase() === item.city_name.toLowerCase()) {
-      return item;
-    };
-  })
-
-  try{
-    let forecast = cityData.data.map(item=>{
-      return new ForeCast(item);
-    })
-    res.send(forecast)
-
-  }
-
-  catch{
-    res.status(500).send('we dont have this city');
-  }
-})  
-
 
 app.get('/weather', weatherHandler)
+
 app.get('/movies', moviesHandler)
 
 
-function weatherHandler(req,res){
-  let lat=req.query.lat
-  let lon=req.query.lon
-  let weatherbackUrl = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_BER_API}&lat=${lat}&lon=${lon}`
-
-  axios.get(weatherbackUrl).then(result1 => {
-
-      let forecastArr = result1.data.data.map(item => {
-          return new ForeCast(item);
-      })
-      res.json(forecastArr);
-
-  })
-  .catch(error => {
-      res.status(500).send(`error in weather data ${error}`);
-  });
-}
-
-
-function moviesHandler(req, res) {
-
-  let moviesKey = process.env.MOVIE_API_KEY;
-  let movieCityName = req.query.city_name;
-  
-  const movieBackUrl = `https://api.themoviedb.org/3/search/movie?api_key=${moviesKey}&query=${movieCityName}`;
-  
-  axios
-      .get(movieBackUrl)
-      .then(movieResult => {
-
-          let moviesArr = movieResult.data.results.map(item => {
-              return new Movie(item);
-          })
-          res.send(moviesArr);
-
-      })
-      .catch(error => {
-          res.status(500).send(`error in movie data ${error}`);
-      });
-
-};
-
-
-
-// app.get('/weather', (req, res) => {
-  
-//   let searchQuery = req.query.city_name;
-//   console.log(searchQuery);
-//   let cityData= weatherData.find((item)=>{
-//     if (searchQuery.toLowerCase() === item.city_name.toLowerCase()) {
-//       return item;
-//     };
-//   })
-
-//   try{
-//     let forecast = cityData.data.map(item=>{
-//       return new ForeCast(item);
-//     })
-//     res.send(forecast)
-
-//   }
-
-//   catch{
-//     res.status(500).send('we dont have this city');
-//   }
-// })
-
-//http://localhost:8000/weather?city_name=Amman
 
 app.get('*', (req, res) => {
   res.status(404).send('Something went wrong');
@@ -148,3 +32,34 @@ app.get('*', (req, res) => {
 
 
 app.listen(process.env.PORT) // kick start the express server to work
+
+
+
+
+
+// http://localhost:8000/weather?city_name=Amman
+
+
+
+// function weatherHandler(req,res){
+//   let lat=req.query.lat
+//   let lon=req.query.lon
+//   let weatherbackUrl = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_BER_API}&lat=${lat}&lon=${lon}`
+
+//   axios.get(weatherbackUrl).then(result1 => {
+
+//       let forecastArr = result1.data.data.map(item => {
+//           return new ForeCast(item);
+//       })
+//       res.json(forecastArr);
+
+//   })
+//   .catch(error => {
+//       res.status(500).send(`error in weather data ${error}`);
+//   });
+// }
+
+
+// a server endpoint 
+// // inside your callback function
+// axios.get(url).then(response => response.data).catch(error => console.log(error));
